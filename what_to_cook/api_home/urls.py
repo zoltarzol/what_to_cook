697a3eca2485f2b9f52db8cde5c15d11a213c4df
Cedric Duroisin <duroisin@gmail.com>
2022-10-28 14:40:51 +0200

    boom

--- what_to_cook/api_home/urls.py
+++ what_to_cook/api_home/urls.py
@@ -5,7 +5,7 @@ app_name = "api_home"
 
 urlpatterns = [
     path('test', views.login),   #api est nom de l'application definit sur views
-    path('home/', views.page_home, name ='home'),
+    path('', views.page_home, name ='home'),
     path('api/', views.create_recipe, name ='api'),
     #path('special/', views.create_recipe, name='special'),
     path('signup/', views.SignupPage.as_view(), name='signup'),
