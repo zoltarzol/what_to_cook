fd93f38cae71584c70465aecfa9dec21023262d8
Emmanuel DEVRIES <hammers178@hotmail.com>
2022-10-31 10:51:03 +0100

    newtest

--- what_to_cook/api_home/urls.py
+++ what_to_cook/api_home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = "api_home"
 
@@ -8,5 +11,7 @@ urlpatterns = [
     path ('', views.base),
     path('asking/', views.aask),
     path('recipe/', views.recipe_rslt),
+    
 ]
+urlpatterns += staticfiles_urlpatterns()
 
