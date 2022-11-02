f386b87bcbeb95a14872b44a761806b5a476f89e
Noura Ous <noura.ousfia@gmail.com>
2022-11-02 15:46:10 +0100

    finalisation authetification

--- what_to_cook/api_home/urls.py
+++ what_to_cook/api_home/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "api_home"
 
 urlpatterns = [
-    path('test', views.login),   #api est nom de l'application definit sur views
+    path('test/', views.login),   #api est nom de l'application definit sur views
     path('', views.page_home, name ='home'),
     path('api/', views.create_recipe, name ='api'),
     #path('special/', views.create_recipe, name='special'),
--- what_to_cook/what_to_cook/settings.py
+++ what_to_cook/what_to_cook/settings.py
@@ -133,3 +133,19 @@ print(STATIC_URL)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/api'
+
+LOGOUT_REDIRECT_URL = '/'
+
+#EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
+
+EMAIL_HOST_USER = "dj.whattocook@gmail.com"   #your account gmail
+EMAIL_HOST_PASSWORD = "ikumehwbkmpevtbu"         #application code
