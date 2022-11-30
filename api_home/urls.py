986f028d4cb22afb1cfcbaaf9a38cb3259dcc8ed
zoltarzol <duroisin@gmail.com>
2022-11-30 14:52:05 +0100

    test

--- api_home/urls.py
+++ api_home/urls.py
@@ -6,7 +6,6 @@ app_name = "api_home"
 urlpatterns = [
     path('', views.page_home, name ='home'),
     path('api/', views.create_recipe, name ='api'),
-    #path('special/', views.create_recipe, name='special'),
     path('signup/', views.SignupPage.as_view(), name='signup'),
     path('recipe/', views.recipe_rslt),
 ]
--- api_home/views.py
+++ api_home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import IngredientsForm, TestForm
 from .functions import ingredients_full_list, find_recipe
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from . import forms
